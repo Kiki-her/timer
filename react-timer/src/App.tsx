3fd900363064d4ea5ca59c75639ae9fccd7facec
kiki <k.kazaneko@gmail.com>
2023-02-24 10:01:19 +0900

    add basic timer

--- react-timer/src/App.tsx
+++ react-timer/src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Timer from "./Timer";
 
 function App() {
+  const time = new Date()
+  time.setSeconds(time.getSeconds() + 600);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Timer expiryTimestamp={time as unknown as number} />
       </header>
     </div>
   );
